6cd6ba7730f01eb81465c37cf0b2412a4a6bb288
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2023-12-31 01:23:13 +0700

    Complete ep04 simple resource crud

---
+++ app/Models/Category.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'image_path',
+    ];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::saving(function ($record) {
+            if ($record->isDirty('image_path')) {
+                $oldImagePath = $record->getOriginal('image_path');
+                self::deleteImage($oldImagePath);
+            }
+        });
+
+        static::deleting(function ($record) {
+            $imagePath = $record->image_path;
+            self::deleteImage($imagePath);
+        });
+    }
+
+    public static function deleteImage($imagePath)
+    {
+        if ($imagePath) {
+            Storage::disk('public')->delete($imagePath);
+        }
+    }
+    
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+}
---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = $this->faker->unique()->words(2, true);
+        
+        return [
+            'name' => $name,
+            'slug' => \Illuminate\Support\Str::slug($name),
+            'price' => $this->faker->numberBetween(100, 1000),
+        ];
+    }
+}
